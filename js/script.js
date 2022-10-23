4d544f75519ffd1c5ea1dcc4dcb2e4c7746c68f7
harrymuliawan03 <harrymuliawan03@gmail.com>
2022-10-23 22:03:33 +0700

    fix bug

--- js/script.js
+++ js/script.js
@@ -22,6 +22,10 @@ document.querySelector('#menu-btn').onclick = () =>{
 //     searchForm.classList.remove('active');
 // }
 
+navbar.onclick = () => {
+    navbar.classList.remove('active');
+}
+
 window.onscroll = () =>{
     navbar.classList.remove('active');
     // searchForm.classList.remove('active');
